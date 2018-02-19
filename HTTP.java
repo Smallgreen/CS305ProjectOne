5dd34ccd1f725e2b021174e6f9a5ebd0887c27e7
wangme <wangme@lafayette.edu>
2018-02-19 15:05:31 -0500

    create html fixing non persistent

--- HTTP.java
+++ HTTP.java
@@ -1,7 +1,15 @@
 public class HTTP {
 
 
-    public HTTP(boolean request, String function,double version, String html){
+    public HTTP(boolean request, String function,double version, String html) {
 
+    }
+
+    public String getRequest(){
+        return null;
+    }
+
+    public String getResponse(){
+        return null;
     }
 }
